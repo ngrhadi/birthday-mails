52edd50574c94053073d4fdf92f83ef0b74b9862
ngrhadi <adinugraha.ista@yahoo.com>
2023-01-28 16:14:49 +0700

    fixing local minute specify

--- utils/jobRun.js
+++ utils/jobRun.js
@@ -12,50 +12,50 @@ const transporter = nodemailer.createTransport({
   }
 })
 
-async function sendMail(user, port) {
-  const options = {
-    header: "Testing",
-    from: 'jhos-gandos@hotmail.com',
-    to: user.email,
-    subject: 'Happy Birthday',
-    text: `Hey, ${user.firsName + ' ' + user.lastName} Happy birthday`
+async function handleEmail(result) {
+  try {
+    const options = {
+      header: "Testing",
+      from: 'jhos-gandos@hotmail.com',
+      to: result.email,
+      subject: 'Happy Birthday',
+      text: `Hey, ${result.firstName + ' ' + result.lastName} Happy birthday`
+    }
+    return transporter.sendMail(options, (err, info) => info.response)
+  } catch (error) {
+    console.log(error)
   }
+}
 
+async function sendMail(user, port) {
   const timestamp = Date.now();
-  const point = user.location; // [latitude,longitude]
+  const point = user.location; // [longitude, latitude]
   const getLocalDate = ts.getFuzzyLocalTimeFromPoint(timestamp, point);
   let localHour = parseInt(moment(getLocalDate).format('HH'))
+  let localMinutes = parseInt(moment(getLocalDate).format('mm'))
+  let dataUser = []
+
+  console.log(user.firstName, localHour, localMinutes)
 
 
   const bodyResponse = {
     email: user.email,
-    message: `Hey, ${user.firsName + ' ' + user.lastName} Happy birthday`
+    message: `Hey, ${user.firstName + ' ' + user.lastName} Happy birthday`
   }
-  if (localHour === 9) {
-    let running = true
-    // setTimeout(() => {
-    const send = transporter.sendMail(options, async (err, info) => {
-      try {
-        const data = info.response
-        const response = await fetch(`https://email-service.digitalenvision.com.au/send-email`, {
-          method: 'POST',
-          body: JSON.stringify(bodyResponse),
-          headers: { 'Content-Type': 'application/json' }
-        })
-        await response.json()
-        return data
-      } catch (error) {
-        console.log("email send not rendering")
-        throw new Error(err)
-      }
+
+  if (localHour === 10 && localMinutes === 30) {
+    const response = await fetch(`https://email-service.digitalenvision.com.au/send-email`, {
+      method: 'POST',
+      body: JSON.stringify(bodyResponse),
+      headers: { 'Content-Type': 'application/json' }
     })
-    running = false
-    return send
-    // }, 90000);
+    await response.json()
+
+    handleEmail(user)
   } else if (localHour < 9) {
     console.log(`is someone have birthday, ready to send at 9am`)
   } else if (localHour > 9) {
-    console.log("is to late for saying birthday")
+    console.log("is to late for saying birthday for", user.firstName)
   }
 
 }
@@ -97,7 +97,7 @@ function initScheduledJobs(port, isUsingGetUser) {
     const scheduledJobFunction = cron.schedule("*/30 * * * *", async () => {
       const response = await fetch(`http://localhost:${port}/users`);
       const data = await response.json();
-      // console.log("using runtime 30 minutes")
+      console.log("using runtime 30 minutes")
       // console.log("using runtime 30 minutes", isTrigered, new Date(new Date().getTime()))
       return validateUser(data, scheduledJobFunction, port)
     });
